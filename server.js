de15df829afe9fbe957e156cf573527d34a1cf1a
Hands-on-robotics <handsonevanoff@gmail.com>
2023-11-23 14:32:30 -0800

    made html and css changes

--- server.js
+++ server.js
@@ -85,7 +85,7 @@ app.get('/login/:id', (req, res) => {
 
       const templateVars = {
         featuredItems: featuredItems,
-        allItems: allItems
+        listedItems: allItems
       };
       console.log(templateVars);
       res.render('index', templateVars);
