76967a70506c5258f126c9085c20d902aeaa791b
Nathan <nathanrmsmith@gmail.com>
2023-11-23 17:10:10 -0500

    Re-seeded db with items photos, featured, and user photos

--- db/seeds/seeds_users.sql
+++ db/seeds/seeds_users.sql
@@ -1,15 +1,15 @@
 -- Users table seeds here (Example)
 INSERT INTO users (seller, name, email, password, profile_url)
-VALUES (FALSE, 'John Doe', 'john.doe@example.com', 'password123', 'https://example.com/profiles/johndoe.jpg');
+VALUES (FALSE, 'John Doe', 'john.doe@example.com', 'password123', '/images/user-images/person-1.png');
 
 INSERT INTO users (seller, name, email, password, profile_url)
-VALUES (TRUE, 'Jane Smith', 'jane.smith@example.com', 'password456', 'https://example.com/profiles/janesmith.jpg');
+VALUES (TRUE, 'Jane Smith', 'jane.smith@example.com', 'password456', '/images/user-images/person-2.png');
 
 INSERT INTO users (seller, name, email, password, profile_url)
-VALUES (FALSE, 'Alice Johnson', 'alice.johnson@example.com', 'password789', 'https://example.com/profiles/alicejohnson.jpg');
+VALUES (FALSE, 'Alice Johnson', 'alice.johnson@example.com', 'password789', '/images/user-images/person-3.png');
 
 INSERT INTO users (seller, name, email, password, profile_url)
-VALUES (TRUE, 'Bob Williams', 'bob.williams@example.com', 'password101', 'https://example.com/profiles/bobwilliams.jpg');
+VALUES (TRUE, 'Bob Williams', 'bob.williams@example.com', 'password101', '/images/user-images/person-4.png');
 
 INSERT INTO users (seller, name, email, password, profile_url)
-VALUES (FALSE, 'Emily Brown', 'emily.brown@example.com', 'password102', 'https://example.com/profiles/emilybrown.jpg');
+VALUES (FALSE, 'Emily Brown', 'emily.brown@example.com', 'password102', '/images/user-images/person-5.png');
--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/login/:id', (req, res) => {
 
     const templateVars = {
       featuredItems: featuredItems,
-      allItems: allItems
+      listedItems: allItems
     }
     console.log(templateVars);
     res.render('index', templateVars)
