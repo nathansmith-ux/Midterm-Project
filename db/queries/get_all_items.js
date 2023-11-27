23c11210d77389ed8af201d1cc05753d1f0a8c4a
Hands-on-robotics <handsonevanoff@gmail.com>
2023-11-27 12:11:18 -0800

    add JOIN for itemsand users to get_all_items.js and get_featured_items.js

--- db/queries/get_all_items.js
+++ db/queries/get_all_items.js
@@ -1,7 +1,7 @@
 const db = require('../connection');
 
 const getAllItems = () => {
-  return db.query(`SELECT * FROM items WHERE featured = false;`)
+  return db.query(`SELECT * FROM items JOIN users ON items.seller_id = users.id WHERE featured = false;`)
     .then(data => data.rows)
     .catch(err => console.log('SQL error', err));
 };
