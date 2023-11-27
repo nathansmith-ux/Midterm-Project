0566aaf5857cceef921bc5ea1debb96adb256da3
Nathan <nathanrmsmith@gmail.com>
2023-11-27 14:10:22 -0500

    Modify db query to be messages obj and correct rows

--- db/queries/add_message.js
+++ db/queries/add_message.js
@@ -1,9 +1,9 @@
 const db = require('../connection');
 
-const addMessage = (senderId, sellerId, content) => {
+const addMessage = (message) => {
   const sendDate = new Date();
-  const queryString = `INSERT INTO messages (seller_id, seller_id, content, send_date, read) VALUES ($1, $2, $3, $4, $5) RETURNING *;`
-  const queryParams = [senderId, sellerId, content];
+  const queryString = `INSERT INTO messages (sender_id, receiver_id, item_id, send_date, content) VALUES ($1, $2, $3, $4, $5) RETURNING *;`
+  const queryParams = [message.senderId, message.sellerId, message.item_id, sendDate, message.content];
 
   return db.query(queryString, queryParams)
     .then(data => data.rows)
--- routes/api/messages-api.js
+++ routes/api/messages-api.js
@@ -3,6 +3,7 @@ const router  = express.Router();
 const messages = require('../../db/queries/add_message');
 
 router.post('/', (req, res) => {
+  const senderId = req.cookies.user_id;
 
 })
 
