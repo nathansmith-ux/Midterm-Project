e370e33e0c52edda51c45d2acff10a4cf8cf3ba4
Nathan <nathanrmsmith@gmail.com>
2023-11-27 13:56:02 -0500

    Add html front end and event listener for jquery

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
   const socket = io();
+
+  $("#send-button").on('click', function(event) {
+    event.preventDefault();
+
+    const $messageInput = $('#message-input').val()
+    const $sellerId = $('#seller-id').val()
+    console.log($messageInput, $sellerId);
+
+  })
+
 })
