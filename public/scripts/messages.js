54ca7708b40154b33b40d5be0879cc215faee712
Nathan <nathanrmsmith@gmail.com>
2023-11-27 19:46:14 -0500

    Re order client side js

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-
   const socket = io();
 
   socket.on('connect', () => {
-
     socket.on("welcome-message", (message) => {
       console.log(message);
-    })
+    });
 
     function getUserId() {
       $.ajax({
@@ -15,42 +13,46 @@ $(document).ready(function() {
       })
       .done(function(response) {
         const userId = response.user[0].id;
-        console.log('You are connected with user id', userId, 'and socket id', socket.id)
-
-        socket.emit('login', { userId: userId, socketId: socket.id})
-      })
+        console.log('You are connected with user id', userId, 'and socket id', socket.id);
+        socket.emit('login', { userId: userId, socketId: socket.id });
+      });
     }
 
     getUserId();
-  })
 
-  $("#send-button").on('click', function(event) {
-    event.preventDefault();
+    $("#send-button").on('click', function(event) {
+      event.preventDefault();
 
-    const $messageInput = $('#message-input').val();
-    const $sellerId = $('#seller-id').val();
-    const $itemId = $('#item-id').val();
+      const $messageInput = $('#message-input').val();
+      const $sellerId = $('#seller-id').val();
+      const $itemId = $('#item-id').val();
 
-    const message = {
-      receiverId: $sellerId,
-      itemId: $itemId,
-      content: $messageInput
-    };
+      const message = {
+        receiverId: $sellerId,
+        itemId: $itemId,
+        content: $messageInput
+      };
 
-    $.ajax({
-      type: "POST",
-      url: '/api/messages',
-      data: message
-    })
-    .done(function(response) {
-      console.log(response)
+      $.ajax({
+        type: "POST",
+        url: '/api/messages',
+        data: message
+      })
+      .done(function(response) {
+        console.log(response);
 
-      function sendMessageToUser(recipientUserId, message) {
-        socket.emit('send message', { to: recipientUserId, message: message });
-      }
+        function sendMessageToUser(recipientUserId, message) {
+          socket.emit('send message', { to: recipientUserId, message: message });
+        }
 
-      sendMessageToUser($sellerId, $messageInput)
-    })
+        sendMessageToUser($sellerId, $messageInput);
+      });
+    });
 
-  })
-})
+    socket.on('receive message', function(data) {
+      const messageContainer = $('#all-messages');
+      const messageItem = $('<p>').text(`${data.from}: ${data.message}`);
+      messageContainer.append(messageItem);
+    });
+  });
+});
