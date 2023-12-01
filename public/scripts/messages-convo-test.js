8864efe1c64a2f5219e0ffe09825dcc3a1e59d5e
Nathan <nathanrmsmith@gmail.com>
2023-11-30 19:35:25 -0500

    Add function for sending message

--- public/scripts/messages-convo-test.js
+++ public/scripts/messages-convo-test.js
@@ -1,25 +1,31 @@
 $(document).ready(() => {
   const socket = io();
 
-  // Getting user cookie
+  // Getting user cookie --> Becomes Sender_id
   const cookies = document.cookie.split('; ');
   const userIdCookie = cookies.find(row => row.startsWith('user_id='));
-  const userId = userIdCookie ? userIdCookie.split('=')[1] : null;
-  console.log(userId);
+  const senderId = userIdCookie ? userIdCookie.split('=')[1] : null;
+  console.log(senderId);
 
   /**
    * Gets the message information that will be sent
+   * Returns obj that containers the message and sender id (cookie id)
    */
   const sendMessage = () => {
     $('#reply-button').on('click', (e) => {
       e.preventDefault();
-      const message = $('#response-message').val()
 
-      console.log(message)
+      const message = $('#response-message').val()
+      const senderInformation = {
+        message: message,
+        senderId: senderId
+      }
+
+      console.log(senderInformation)
     })
   }
 
-  sendMessage();
+  
 
 
   socket.on("connect", () => {
